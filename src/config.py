49cb8d3b17385bd03267c244aa791be334456897
theolaos <92591390+theolaos@users.noreply.github.com>
2026-08-12 17:04:21 +0300

    Added license notice in every code file

--- src/config.py
+++ src/config.py
@@ -1,12 +1,20 @@
+# CoulombHelper: A simple electronic physics simulator
+# Copyright (C) 2026  theolaos
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #CONFIG FILE FOR THE COULOMB HELPER
 class Config:
     k: float = 8.98755*10**9
     debug: bool = False
-
-
-def print_debug( *kargs, sep: str = " ", endl: str = "\n" ):
-    if Config.debug is True:
-        for i in kargs:
-            print( i, end=sep )    
-        print( end=endl )
-
