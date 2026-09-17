130807594ebe0d1f09b0709f9c7bce215c9b2b54
theolaos <92591390+theolaos@users.noreply.github.com>
2026-09-17 21:36:15 +0300

    Renamed the file to Constants

--- src/constants.py
+++ src/constants.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#CONFIG FILE FOR THE COULOMB HELPER
-class Config:
-    k: float = 8.98755*10**9
-    debug: bool = False
+k: float = 8.98755*10**9
+
+milli: float = 10**(-3)
+micro: float = 10**(-6)
+nano: float = 10**(-9)
